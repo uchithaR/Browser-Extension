fd776d7c2b7ce06fde14a0d82f317ddddda6ddf2
UchithaR <uchitha17669@gmail.com>
2017-03-04 16:24:56 +0530

    initial commit

---
+++ over_view.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  
+  chrome.windows.getAll({populate:true}, getAllOpenWindows);
+
+function getAllOpenWindows(winData) {
+
+  var tabs = [];
+  for (var i in winData) {
+    if (winData[i].focused === true) {
+        var winTabs = winData[i].tabs;
+        var totTabs = winTabs.length;
+        for (var j=0; j<totTabs;j++) {
+          tabs.push(winTabs[j].url);
+        }
+    }
+  }
+  console.log(tabs);
+}
+}, false);
